0c0769798e4c6fc26b9ca74326a1aa8feb3fb350
Rahul <rahulsharma3258@gmail.com>
2023-06-11 19:22:52 +0530

    changed to const

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
